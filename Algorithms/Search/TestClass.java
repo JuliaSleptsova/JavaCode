f9bc213242869aa237928747f2c8f58e204153a0
Julia Sleptsova <yulik1995@gmail.com>
2020-08-30 09:25:03 +0300

    add SelectSotr and test

--- Algorithms/Search/TestClass.java
+++ Algorithms/Search/TestClass.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class TestClass {
     public static void main(String[] args) {
-        int index1 = BinarySearch.binarySearch(new int[]{1, 33, 45, 123, 897, 1002, 70009}, 89);
-        print(89, index1);
+        int index1 = BinarySearch.binarySearch(new int[]{1, 33, 45, 123, 897, 1002, 70009}, 70009);
+        print(70009, index1);
         int index2 = LinearSearch.linSearch(new int[]{1, 45, 23, 4, 89, 8}, 4);
         print(index2, 4);
+
+        int[] testSelSort = SelectionSort.selecSort(new int[]{2, 44444, 1, 34, 23, 56, 45, 9});
+        System.out.println(Arrays.toString(testSelSort));
     }
 
     public static void print(int elementKey, int index) {
@@ -13,4 +18,5 @@ public class TestClass {
             System.out.println(elementKey + " found at index: " + index);
         }
     }
+
 }
--- Algorithms/Sort/SelectionSort.java
+++ Algorithms/Sort/SelectionSort.java
@@ -1,5 +1,19 @@
 public class SelectionSort {
-    public static void main(String[] args){
+    public static int[] selecSort(int[] arr){
+    for (int i = 0; i < arr.length; i++){
+        int min = arr[i];
+        int minId = i;
+        for (int j = i + 1; j < arr.length; j++) {
+            if (arr[j] < min ){
+                min = arr[j];
+                minId = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = min;
+        arr[minId] = temp;
 
+    }
+    return arr;
     }
 }
