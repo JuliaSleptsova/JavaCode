e0ce3c5ce7727f21da95aabf4b5836872d7fb597
Julia Sleptsova <yulik1995@gmail.com>
2020-09-05 19:01:37 +0300

    add class min 3 numbers

---
+++ LearningTask/min3numbers.java
@@ -0,0 +1,30 @@
+public class min3numbers {
+    public static int min(int a, int b, int c) {
+
+        int min;
+        if (a > b){
+            if (c > b){
+                min = b;
+            }
+            else {
+                min = c;
+            }
+        }
+        else {
+            if (a> c){
+                min = c;
+            }
+            else {
+                min  = a;
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
+}
