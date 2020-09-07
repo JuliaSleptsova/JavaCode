0d69f925271c5eaf21fc42c74c26d23867ed5b05
Julia Sleptsova <yulik1995@gmail.com>
2020-09-07 11:43:02 +0300

    add input description

--- JavaRushTacks/1.JavaSyntax/Hello.java
+++ JavaRushTacks/1.JavaSyntax/Hello.java
@@ -4,8 +4,10 @@ import java.io.InputStreamReader;
 public class Hello {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String sAge = reader.readLine();
+        System.out.println("What is your name?");
         String name = reader.readLine();
+        String sAge = reader.readLine();
+
 
         int nAge = Integer.parseInt(sAge);
         System.out.println("My name is "  + name + ". "+"I'm "+ nAge + " years old." );
