7e4618d99cd0f0b24abc4c323f8bd6e5bc6a0bdc
Julia Sleptsova <yulik1995@gmail.com>
2020-09-09 18:48:11 +0300

    added a condition for assigning the maximum value

--- JavaRushTacks/1.JavaSyntax/MaxOfEnterNumbers.java
+++ JavaRushTacks/1.JavaSyntax/MaxOfEnterNumbers.java
@@ -4,13 +4,14 @@ import java.io.InputStreamReader;
 public class MaxOfEnterNumbers {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = -2147483648;
+        int maximum = 0;
         String sNum = reader.readLine();
         int N = Integer.parseInt(sNum);
         if (N > 0){
-            for (int i = 0; i < N; i++){
+            for (int i = 0; i < N; i++) {
                 String sNumber = reader.readLine();
                 int n = Integer.parseInt(sNumber);
+                if (i==0) maximum = n;
                 if (n > maximum){
                     maximum = n;
                 }
