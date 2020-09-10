ab62ceb7fe8b9f3a98f125c7296ac5a54c3386c6
Julia Sleptsova <yulik1995@gmail.com>
2020-09-10 18:35:05 +0300

    rename class

--- JavaRushTacks/1.JavaSyntax/MaxOfValueInArray.java
+++ JavaRushTacks/1.JavaSyntax/MaxOfValueInArray.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class MaxValueOfArray {
+public class MaxOfValueInArray {
     public static void main(String[] args) throws Exception {
         int[] array = initializeArray();
         int max = max(array);
